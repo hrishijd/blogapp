ea9701b24c9250c240b4bb1301d2c8b3576e066b
hrishijd <hrishi.jadhav24@gmail.com>
2020-01-13 21:25:54 +0530

    working templates -2

--- src/blogapp/blog/views.py
+++ src/blogapp/blog/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+posts = [
+	{
+		'author'		:'hrishi jadhav',
+		'title'			:'blog post 1',
+		'content'		:'First post content',
+		'date_posted'	:'january 13,2020'
+	},
+	{
+		'author'		:'corey schafer',
+		'title'			:'blog post 2',
+		'content'		:'Second post content',
+		'date_posted'	:'february 13,2020'
+	}
+]
 def Home(request):
-	return render(request, 'blog/home.html')
+	context={
+		'posts':posts
+	}
+	return render(request, 'blog/home.html',context)
 def About(request):
-	return 	render(request, 'blog/about.html')
+	return render(request, 'blog/about.html')
